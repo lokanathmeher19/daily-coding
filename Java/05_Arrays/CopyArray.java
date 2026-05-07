6b880a23a99ec5a21fdff6f2f06dcfa36165629b
Lokanath Meher <meherlokanath314@gmail.com>
2026-05-07 22:54:59 +0530

    java

--- Java/05_Arrays/CopyArray.java
+++ Java/05_Arrays/CopyArray.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class CopyArray {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// public class CopyArray {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int[] copy = new int[n];
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         int[] copy = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            copy[i] = arr[i];
-        }
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//             copy[i] = arr[i];
+//         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(copy[i] + " ");
-        }
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(copy[i] + " ");
+//         }
 
-        sc.close();
-    }
-}
+//         sc.close();
+//     }
+// }
+
+
+// # java progrmmin 
