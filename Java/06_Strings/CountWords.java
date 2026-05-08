7c428f92bd69ebb60bb9ee6af3737ae6fc0b70a1
Lokanath Meher <meherlokanath314@gmail.com>
2026-05-08 23:41:38 +0530

    java

---
+++ Java/06_Strings/CountWords.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine().trim();
+
+        String[] words = str.split("\\s+");
+
+        System.out.println("Words = " + words.length);
+
+        sc.close();
+    }
+}
