307e6ebbb41c7f111fcfb73c7cc0f6251e789d3e
Lokanath Meher <meherlokanath314@gmail.com>
2026-05-11 23:20:40 +0530

    java

---
+++ Java/06_Strings/CharacterFrequency.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        char target = sc.next().charAt(0);
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Frequency = " + count);
+
+        sc.close();
+    }
+}
