8417b2b2f7dbb0404ee542f681cd322fc08d9d50
Lokanath Meher <meherlokanath314@gmail.com>
2026-05-12 22:58:47 +0530

    java

---
+++ Java/07_Methods/AddMethod.java
@@ -0,0 +1,12 @@
+public class AddMethod {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(10, 5);
+
+        System.out.println("Sum = " + result);
+    }
+}
