887ee6b70c62d70791af85c42d772345502bab22
Lokanath Meher <meherlokanath314@gmail.com>
2026-07-04 21:07:35 +0530

    07_Method java

---
+++ Java/07_Methods/FactorialMethod.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FactorialMethod {
+
+    static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+
+        sc.close();
+    }
+}
